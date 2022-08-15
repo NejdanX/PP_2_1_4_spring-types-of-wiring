662b107649c5d1189f28be6702edd26c32563ddf
Gennadii Plahonin <plahonin99@mail.ru>
2022-08-15 20:53:53 +0900

    Set all beans

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
+
 public class Egg6 {
+
+    final private Needle7 needle;
+
+    public Egg6(Needle7 needle) {
+        // Через конструктор
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
