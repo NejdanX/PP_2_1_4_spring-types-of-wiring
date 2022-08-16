aa830d099ea1054a00eb39c05c4d8c35205c2816
Gennadii Plahonin <plahonin99@mail.ru>
2022-08-16 10:56:44 +0900

    Added Needle and Death dependencies

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -6,8 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
+    private Deth8 death;
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :(";
+        return ", смерть Кощея на игле :(" + death;
+    }
+
+    @Autowired
+    public void setDeath(Deth8 death) {
+        this.death = death;
     }
 }
